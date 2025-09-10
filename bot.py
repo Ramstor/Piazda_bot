43eeeb7289d7152687e0fca14bbf18be04ee5710
Ramstor <39264037+Ramstor@users.noreply.github.com>
2025-09-10 17:01:08 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(lambda message: message.text and message.text.strip().lower() == "да")
+@dp.message(F.text.regexp(r"(?i)^да$"))
 async def reply_da(message: types.Message):
-    await message.reply("пизда")
+    await message.reply("пязда")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
