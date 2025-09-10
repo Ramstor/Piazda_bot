80f110d94ec5d8d73a2ba7a4b8308473449bf4af
Ramstor <39264037+Ramstor@users.noreply.github.com>
2025-09-10 16:36:54 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,30 +1,19 @@
 import logging
+from aiogram import Bot, Dispatcher, executor, types
 import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Text
-from aiogram import F
 
-# Логирование
-logging.basicConfig(level=logging.INFO)
-
-# Берём токен из переменной окружения
+# Берём токен из переменных окружения (удобно для хостинга)
 API_TOKEN = os.getenv("7881272979:AAEKnpHPz5fT-XhBqJmopaNXOZjjeNDrdro")
 
-# Создаём объекты
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+logging.basicConfig(level=logging.INFO)
 
-# Хэндлер на слово "да"
-@dp.message(F.text.lower() == "да")
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(lambda message: message.text and message.text.strip().lower() == "да")
 async def reply_da(message: types.Message):
     await message.reply("пизда")
 
-# Точка входа
-async def main():
-    await dp.start_polling(bot)
-
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
-
+    executor.start_polling(dp, skip_updates=True)
 
